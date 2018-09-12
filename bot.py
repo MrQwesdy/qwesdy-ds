f8ce24f803785e4ec0d514c5e5bd47e0bf5fe0c0
MrQwesdy <35886301+MrQwesdy@users.noreply.github.com>
2018-09-12 15:30:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,29 +16,8 @@ hour = 0
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name='.help | By Qwesdy',type=3))
-    bot.add_cog(Uptime(bot))
     print('Connected')
 
-class Uptime:
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(pass_context=True)
-    async def uptime(self, ctx):
-        current_time = time.time()
-        difference = int(round(current_time - start_time))
-        text = str(datetime.timedelta(seconds=difference))
-        embed = discord.Embed(colour=ctx.message.author.top_role.colour)
-        embed.add_field(name="Uptime", value=text)
-        embed.set_footer(text="Sponsored by altcointrain.com - Choo!!! Choo!!!")
-        try:
-            await self.bot.say(embed=embed)
-        except discord.HTTPException:
-            await self.bot.say("Current uptime: " + text)
-
-
-    
-    
 # Testing
 
 @client.command(pass_context = True)
@@ -99,7 +78,7 @@ async def ig():
 
 @client.command()
 async def qwenty():
-    await client.say("Qwenty's Discord » `https://discordapp.com/invite/cFEfmNh`")
+    await client.say("Qwenty's Discord » `https://discord.gg/gmh8ay`")
 
 @client.command()
 async def dev():
