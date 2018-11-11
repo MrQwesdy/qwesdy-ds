cf2a6f316212f3998195447a17637dfd5fb9cb9e
Qwesdy <35886301+MrQwesdy@users.noreply.github.com>
2018-11-11 12:58:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -121,4 +121,25 @@ async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='Member')
     await client.add_roles(member, role)
 
+# Audio
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+
+@client.command(pass=context=True)
+asnyc def play(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
+    
 client.run(TOKEN)
