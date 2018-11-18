4e55289fe0670bb7accab9db3c9785061bb2f1f1
Qwesdy <35886301+MrQwesdy@users.noreply.github.com>
2018-11-18 14:37:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,7 +96,6 @@ async def help(ctx):
     embed.add_field(name=".ban (@User) - Permanently IP Bans an chosen User from the server", value="** **", inline=False)
     embed.add_field(name="To unban someone you have to do it manualy through Server Settings!", value="** **", inline=False)
     embed.add_field(name="** **", value="** **", inline=False)
-    embed.add_field(name="Found by Qwenty.#7942", value="** **", inline=False)
     embed.add_field(name="Developed by Qwesdy#9217", value="** **", inline=False)
     embed.add_field(name="** **", value="** **", inline=False)
     embed.add_field(name="Made with :heart:", value="** **", inline=False)
